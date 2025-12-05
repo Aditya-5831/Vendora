c35bcb13e71d9e45ee13717893c83de9f875a40f
Aditya Shiyale <adityashiyale7219@gmail.com>
2025-12-05 19:46:37 +0530

    add Clerk integration, sync user with Inngest, and update Prisma user schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import { ENV } from './config/env.js';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest/express'
+import { functions, inngest } from './config/inngest.js';
 
 // Configuration
 const app = express();
 
 
 // Middlewares
+app.use(express.json())
 app.use(clerkMiddleware())
+app.get("/api/inngest", serve({ client: inngest, functions }))
 
 // Server is running on port 5000   
 app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`));
