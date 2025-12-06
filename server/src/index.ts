0a88614d856ca57fa7464426ed6f38c1d03b3b3a
Aditya Shiyale <adityashiyale7219@gmail.com>
2025-12-06 16:43:48 +0530

    chore: remove temporary health endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,9 +15,6 @@ app.get("/api/inngest", serve({ client: inngest, functions }))
 
 
 // Routes
-app.get("/health", (req, res) => {
-    res.send({ success: true })
-})
 
 // Server is running on port 5000   
 app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`));
