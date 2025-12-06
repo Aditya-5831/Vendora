76f6ffcf942b574155cda0659de7c5691360a140
Aditya Shiyale <adityashiyale7219@gmail.com>
2025-12-06 16:39:43 +0530

    feat: add health endpoint for server status check

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request } from 'express';
 import { ENV } from './config/env.js';
 import { clerkMiddleware } from '@clerk/express';
 import { serve } from 'inngest/express'
@@ -12,6 +12,12 @@ const app = express();
 app.use(express.json())
 app.use(clerkMiddleware())
 app.get("/api/inngest", serve({ client: inngest, functions }))
+
+
+// Routes
+app.get("/health", (req, res) => {
+    res.send({ success: true })
+})
 
 // Server is running on port 5000   
 app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`));
